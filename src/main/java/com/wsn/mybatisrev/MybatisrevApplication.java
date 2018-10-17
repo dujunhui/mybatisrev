8091dcd5428e94ea725e45724c9384b9eb877fe9
dujunhui <dujunhui@qq.com>
2018-10-17 09:47:54 +0800

    Initial commit

---
+++ src/main/java/com/wsn/mybatisrev/MybatisrevApplication.java
@@ -0,0 +1,12 @@
+package com.wsn.mybatisrev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisrevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisrevApplication.class, args);
+    }
+}
---
+++ src/test/java/com/wsn/mybatisrev/MybatisrevApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wsn.mybatisrev;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MybatisrevApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
